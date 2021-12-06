78f3977641bff3af4237d7795f18e6fbaf580b35
farah-ebnhadjali <farah.bhadjali@gmail.com>
2021-12-06 11:44:40 +0100

    Modèle de vue partagé à travers les fragments

---
+++ app/src/main/java/com/farah/cupcake/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.farah.cupcake
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+
+/**
+ * Activity for cupcake order flow.
+ */
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()|| super.onSupportNavigateUp()
+    }
+}
